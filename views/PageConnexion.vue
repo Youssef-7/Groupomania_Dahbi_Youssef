95b2a6bcd34bbce064c52f831f6ac614aa72435a
Youssef-7 <80470335+Youssef-7@users.noreply.github.com>
2022-03-18 14:55:04 +0100

    Add files via upload

---
+++ views/PageConnexion.vue
@@ -0,0 +1,15 @@
+<template>
+    <HelloWorld/>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/PageConnexion.vue'
+
+export default {
+  name: 'PageConnexion',
+  components: {
+    HelloWorld
+  }
+}
+</script>
