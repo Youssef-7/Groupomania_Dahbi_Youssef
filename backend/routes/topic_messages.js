f5fbd8aacef25d245a2961d4e9f4c50a05d6fca0
Youssef-7 <80470335+Youssef-7@users.noreply.github.com>
2022-08-19 12:26:08 +0200

    Add files via upload

--- backend/routes/topic_messages.js
+++ backend/routes/topic_messages.js
@@ -26,22 +26,17 @@ router.post('/topic_messages', auth, multer, topic_messagesCtrl.createMessage)
 // Get all topics (parent 0)// route temporaire sans auth
 router.get('/topic_messages/parent', auth, topic_messagesCtrl.showParentTopicMessages);
 
-// Get all childs where parent_id = 0// route temporaire sans auth
-router.get('/topic_messages/child/:parent_id', auth, topic_messagesCtrl.showChildTopicMessages);
-
 // Update Topic_message to Database// route temporaire sans auth
 router.put('/topic_messages/:p_id', auth, multer, topic_messagesCtrl.updateMessage);
 
-// Update Moderation// route temporaire sans auth
-router.put('/topic_messages/moderation', auth, topic_messagesCtrl.updateModeration);
 
 // Delete Message from Database// route temporaire sans auth
 router.delete('/topic_messages/:p_id', auth, topic_messagesCtrl.deleteMessage);
 
-router.post('/topic_messages/like/', auth, multer, topic_messagesCtrl.createLikeMessage);
+router.post('/topic_messages/like/:p_id', auth, multer, topic_messagesCtrl.createLikeMessage);
 // router.post('/topic_messages/like/:p_id', auth, multer, topic_messagesCtrl.createLikeMessage);
 
-router.delete('/topic_messages/:like_id', auth, multer, topic_messagesCtrl.deleteLikeMessage);
+router.delete('/topic_messages/dislike/:p_id', auth, multer, topic_messagesCtrl.deleteLikeMessage);
 
 // Get message by title
 // router.get('/topic_messages', auth, topic_messagesCtrl.showMessageByTitle);
