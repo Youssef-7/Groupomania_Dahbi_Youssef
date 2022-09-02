e4c968c3d50728e64c90319d6a39987c04cbd367
Youssef-7 <80470335+Youssef-7@users.noreply.github.com>
2022-09-02 11:58:10 +0200

    Add files via upload

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt'); // Algorythme de hachage = package de chiffrem
 
 // Insert into Users in Database table users
 exports.insertUsers = (data, result) => {
+    connection.query("SELECT * FROM usagers WHERE u_email = ?", [data.u_email],(err, results)=> {
+        if (!results[0]) {
     connection.query("INSERT INTO usagers (u_pseudo, u_email, u_pwd) VALUES (?, ?, ?)", [data.u_pseudo, data.u_email, data.u_password], (err, results) => {
         if (err) {
             result(err, null);
@@ -13,6 +15,9 @@ exports.insertUsers = (data, result) => {
             return results;
         }
     });
+    }
+     else { result(null, results); }
+})
 }
 
 // fonction find user by email
@@ -87,17 +92,3 @@ exports.deleteUserById = (id, result) => {
         }
     });
 }
-
-// Query pour accès moderation
-// exports.getUserLevel = (user_id, result) => {
-//     connection.query("GET u_level FROM users WHERE u_id = ?", [user_id], (err, results) => {
-//         if (err) {
-//             console.log("error: ", err);
-//             result(err, null);
-//         }
-//         else {
-//             result(null, results);
-//             return results
-//         }
-//     });
-// }
