bb5df39f02e1274b06737b25f4ee231ff657dbc0
Youssef-7 <80470335+Youssef-7@users.noreply.github.com>
2022-09-05 09:17:29 +0200

    Add files via upload

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken'); // package jsonwebtoken pour vérifier les tokens
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 module.exports = (req, res, next) => {  
  // tryconsole.log(process.env) ...catch pour réduire les erreurs liés à tous les problèmes possibles
     try {
         const token = req.headers.authorization.split(' ')[1]; // on extrait le token du header authorization de la requête entrante (2e élément du tableau retourné avec le bearer et le token)
         const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN_KEY); // verify permet de décoder le token grâce au package jsonwebtoken : si token invalide = génère une erreur
         const userId = decodedToken.userId;
-        const level = decodedToken.u_role; // on extrait l'id utilisateur du token
+        const level = decodedToken.level; // on extrait l'id utilisateur du token
+        req.auth = { userId, level }
         if (req.body.userId && req.body.userId !== userId) { // si la demande contient un id user : on le compare à celui extrait du token
             throw 'ID utilisateur non valable'; // s'ils sont différents, on génère une erreur
         } else {
