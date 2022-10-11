ef15cdc2f59cbb5f16a85b49e4199f57295116a9
Youssef-7 <80470335+Youssef-7@users.noreply.github.com>
2022-10-11 18:33:35 +0200

    Add files via upload

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import PageConnexion from '../views/PageConnexion.vue'
 import PageAccueil from '../views/PageAccueil.vue'
-import PageCommentaire from '../views/PageCommentaire.vue'
+
 
 
 const routes = [
@@ -18,14 +18,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: PageAccueil
   },
-    {
-    path: '/PageCommentaire',
-    name: 'PageCommentaire',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: PageCommentaire
-  }
+
 ]
 
 const router = createRouter({
